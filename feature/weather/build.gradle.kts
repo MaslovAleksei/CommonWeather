691cb94ab36c5538b1d32dd0b82c3b1acb8bf0bb
MaslovAleksei <erectus72@gmail.com>
2024-01-10 02:14:21 +0500

    migrate to Flow

--- feature/weather/build.gradle.kts
+++ feature/weather/build.gradle.kts
@@ -50,8 +50,6 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-
     implementation("com.google.dagger:dagger:2.48.1")
     ksp("com.google.dagger:dagger-compiler:2.48.1")
 
